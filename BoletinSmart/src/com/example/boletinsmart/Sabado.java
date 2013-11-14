ceabcf43997967c93237e81afa2be274f527899d
eliceo <elandradepepito@gmail.com>
2013-11-14 19:01:28 -0430

    actualizado

---
+++ BoletinSmart/src/com/example/boletinsmart/Sabado.java
@@ -0,0 +1,13 @@
+package com.example.boletinsmart;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+
+
+public class Sabado extends Activity{
+	public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.sabado);
+    }
+}
